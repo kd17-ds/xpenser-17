d1e046627d7b6e86ee03c0f98711e7c49ea4cff4
Divyansh Sharma <shan17div@gmail.com>
2025-02-01 15:47:30 +0530

    Welcome Page Basic Design

--- fintrack/src/LandingPage/Welcome/Welcome.jsx
+++ fintrack/src/LandingPage/Welcome/Welcome.jsx
@@ -2,6 +2,8 @@ import styles from "./Welcome.module.css";
 
 export default function Welcome() {
     return (
-        <div></div>
+        <div className={styles.WelBox}>
+            <h1 className={`MainHeading tracking-in-expand ${styles.WelHead} `}> Fintrack</h1>
+        </div>
     )
 }
