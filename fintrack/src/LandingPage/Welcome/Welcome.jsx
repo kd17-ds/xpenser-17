e64535d3bde63feedef2687a3062686d05a61e68
Divyansh Sharma <shan17div@gmail.com>
2025-02-02 11:28:23 +0530

    Animation to a new Page

--- fintrack/src/LandingPage/Welcome/Welcome.jsx
+++ fintrack/src/LandingPage/Welcome/Welcome.jsx
@@ -2,8 +2,14 @@ import styles from "./Welcome.module.css";
 
 export default function Welcome() {
     return (
-        <div className={styles.WelBox}>
-            <h1 className={`MainHeading tracking-in-expand ${styles.WelHead} `}> Fintrack</h1>
+        <div>
+            <div className={styles.WelBox}>
+                <h1 className={`MainHeading tracking-in-expand ${styles.WelHead} `}> Fintrack</h1>
+                <br />
+                <p className={`SubHeading text-focus-in ${styles.WelFont} `}>" Your go-to app for taking control of your finances.  "</p>
+            </div>
+            <div className={` slide-top ${styles.HomeContentBox}`}>
+            </div>
         </div>
     )
 }
