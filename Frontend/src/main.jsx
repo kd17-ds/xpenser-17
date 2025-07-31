eae65f031c4e05ef5e7ab5dabf9be56935ce346f
Divyansh Sharma <shan17div@gmail.com>
2025-07-31 18:50:41 +0530

    Creating PWA

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -16,4 +16,9 @@ createRoot(document.getElementById('root')).render(
   </BrowserRouter>,
 );
 
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(() => console.log('Service Worker registered'))
+    .catch((err) => console.error('Service Worker registration failed:', err));
+}
